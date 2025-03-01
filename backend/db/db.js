563223885aee7e912f7fe6d4e8e1672f03fa5405
Piyushrathoree <meonpath008@gmail.com>
2025-03-02 03:49:43 +0530

    Update database connection logging message

--- backend/db/db.js
+++ backend/db/db.js
@@ -2,7 +2,7 @@ import mongoose from "mongoose";
 
 function connectDB() {
     const URl = `${process.env.MONGO_URL}/${process.env.DB_NAME}`;
-    console.log("Database connected");
+    console.log("Database connected........");
     return mongoose.connect(URl);
 }
 
