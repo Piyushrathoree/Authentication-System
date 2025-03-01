3b837e91062dc2ce6faeadac76fdc107d5792cce
Piyushrathoree <meonpath008@gmail.com>
2025-03-02 03:49:24 +0530

    Configure ES modules and add cookie parsing middleware

--- backend/app.js
+++ backend/app.js
@@ -1,10 +1,12 @@
 import express from "express";
 import bodyParser from "body-parser";
+import cookieParser from "cookie-parser";
 
 const app = express();
 app.use(bodyParser.json());
+app.use(cookieParser());
 
-import router from "./routes/auth.route";
+import router from "./routes/auth.route.js";
 app.use("/api/v1", router);
 
 export default app;
