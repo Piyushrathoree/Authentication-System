99eb107a541397ed2ddcc96da70f88187e22d3ff
Piyushrathoree <meonpath008@gmail.com>
2025-03-02 03:50:01 +0530

    Refactor auth route imports and method references

--- backend/routes/auth.route.js
+++ backend/routes/auth.route.js
@@ -1,10 +1,10 @@
 import { Router } from "express";
-import AuthController from "../controllers/auth.controller";
+import {registerUser , loginUser , logout} from "../controllers/auth.controller.js";
 
 const router = Router();
 
-router.post("/signup", AuthController.signup);
-router.post("/login", AuthController.login);
-router.post("/logout", AuthController.logout);
+router.post("/signup",registerUser);
+router.post("/login", loginUser);
+router.post("/logout", logout);
 
 export default router;
