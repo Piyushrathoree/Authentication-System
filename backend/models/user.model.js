b24486d53b02a7c0d75225b8eb922c8a117f06d2
Piyushrathoree <meonpath008@gmail.com>
2025-03-02 01:26:39 +0530

    Add authentication methods to user model

--- backend/models/user.model.js
+++ backend/models/user.model.js
@@ -1,35 +1,51 @@
 import mongoose from "mongoose";
+import bcrypt from "bcrypt";
+import jwt from "jsonwebtoken";
 
 const userSchema = new mongoose.Schema({
-    name:{
-        firstName:{
+    name: {
+        firstName: {
             type: String,
-            required: true
+            required: true,
         },
-        lastName:{
+        lastName: {
             type: String,
-        }
+        },
     },
-    email:{
+    email: {
         type: String,
         required: true,
-        unique: true
+        unique: true,
     },
-    password:{
+    password: {
         type: String,
         minlength: 8,
         validate: {
-            validator: function(password) {
+            validator: function (password) {
                 // At least 8 chars, 1 uppercase, 1 lowercase, 1 number, 1 special char
-                const strongPasswordRegex = /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{8,}$/;
+                const strongPasswordRegex =
+                    /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{8,}$/;
                 return strongPasswordRegex.test(password);
             },
-            message: 'Password must be at least 8 characters long and contain at least one uppercase letter, one lowercase letter, one number and one special character'
+            message:
+                "Password must be at least 8 characters long and contain at least one uppercase letter, one lowercase letter, one number and one special character",
         },
-        required: true
+        required: true,
     },
+});
+userSchema.static.hashPassword = async function (password) {
+    return await bcrypt.hash(password, 10);
+};
 
-})
+userSchema.methods.generateAuthToken = function () {
+    const token = jwt.sign({ _id: this._id }, process.env.JWT_SECRET, {
+        expiresIn: "24h",
+    });
+    return token;
+};
 
+userSchema.methods.isPasswordCorrect = async function (password) {
+    const password = await bcrypt.compare(password, this.password);
+};
 
-const User = mongoose.model('User', userSchema);
+const User = mongoose.model("User", userSchema);
